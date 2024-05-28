1e0db231865574c599f86aff61a9f4310de3a024
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-05-28 14:51:29 +0800

    test: checks if shadcn works in our project

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
 
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div>
+      <Button variant="outline">Button</Button>
+      <Button variant="outline">Button</Button>
+    </div>
   )
 }
 
