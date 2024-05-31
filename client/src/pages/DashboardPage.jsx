e3dabac342477d32fa737ecae9fc0b2ab8db006a
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-05-31 16:52:49 +0800

    feat(client): Add dashboard layout scaffolding

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,8 +1,83 @@
 import React from 'react'
 
+import Logo from '@/components/logo'
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+import Profile from '@/components/profile'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div className="min-h-screen bg-background">
+      <header className="p-4 flex items-center justify-between border-b-2">
+        <Logo />
+        <Profile />
+      </header>
+
+      <main className="p-8 flex flex-col gap-4 text-left">
+        <div>
+          <h2 className="font-bold text-2xl">Title</h2>
+          <p>Subtitle</p>
+        </div>
+
+        <div>
+          <Table className="border-2">
+            <TableCaption>
+              A list of your recent invoices.
+
+              <Pagination className="mt-2">
+                <PaginationContent>
+                  <PaginationItem>
+                    <PaginationPrevious href="#" />
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationNext href="#" />
+                  </PaginationItem>
+                </PaginationContent>
+              </Pagination>
+            </TableCaption>
+
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Invoice</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Method</TableHead>
+                <TableHead className="text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">INV001</TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+      </main>
+    </div>
   )
 }
 
