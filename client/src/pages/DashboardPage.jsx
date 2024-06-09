bfa03d91c3596796e4e415ab07b405f5a07b293b
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-06-09 17:10:43 +0800

    Add dashboard create event dialog

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import DashboardTable from '@/components/dashboardtable'
-import EventSelectModal from '@/components/EventSelectModal'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { CirclePlus, SlidersHorizontal } from 'lucide-react'
-import Header from '@/components/Header';
+import DashboardTable from "@/components/dashboardtable";
+import EventSelectModal from "@/components/EventSelectModal";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { CirclePlus, SlidersHorizontal } from "lucide-react";
+import Header from "@/components/Header";
+import CreateEventModal from "@/components/CreateEventModal";
 
 const DashboardPage = () => {
-  const [eventFilter, setEventFilter] = useState('');
+  const [eventFilter, setEventFilter] = useState("");
 
+  const [showCreateEventDialog, setShowCreateEventDialog] = useState(false);
   const [showEventSelectModal, setShowEventSelectModal] = useState(false);
 
   // TODO: Pass needed args
@@ -29,15 +31,14 @@ const DashboardPage = () => {
           </div>
 
           <div className="flex justify-between">
-            {/* TODO: Handle event creation */}
-            <Button>
+            <Button onClick={() => setShowCreateEventDialog(true)}>
               <CirclePlus className="mr-2 w-4" /> Create Event
             </Button>
 
             <div className="flex gap-4">
               <Input
                 placeholder="Search event names..."
-                onChange={e => setEventFilter(e.target.value)}
+                onChange={(e) => setEventFilter(e.target.value)}
               />
 
               {/* TODO: Is this really necessary? */}
@@ -47,20 +48,27 @@ const DashboardPage = () => {
             </div>
           </div>
 
-          <DashboardTable 
-            className="border-2" 
-            eventFilter={eventFilter} 
+          <DashboardTable
+            className="border-2"
+            eventFilter={eventFilter}
             onEventSelect={handleEventSelect}
           />
         </main>
       </div>
 
-      <EventSelectModal 
-        show={showEventSelectModal} 
-        onExit={() => setShowEventSelectModal(false)} 
+      <EventSelectModal
+        show={showEventSelectModal}
+        onExit={() => setShowEventSelectModal(false)}
+      />
+
+      {/* TODO: Proper handling of event creation */}
+      <CreateEventModal
+        open={showCreateEventDialog}
+        onOpenChange={setShowCreateEventDialog}
+        onEventCreate={() => setShowCreateEventDialog(false)}
       />
     </>
-  )
-}
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
